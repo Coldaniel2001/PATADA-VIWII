37c69765cbb39653be2620dabe838e96626ef7a5
Iñaki García <igardiet@gmail.com>
2022-12-03 11:45:54 +0100

    update desktop-1-2 inaki

--- script.js
+++ script.js
@@ -3,11 +3,28 @@
 const comenzarPartida = document.querySelector(".comenzar-partida-padre");
 const page1 = document.querySelector(".desktop-1")
 const page2 = document.querySelector(".desktop-2")
+const easyDif = document.querySelectorAll(".easy-dif");
+const easy = document.querySelector('#easy');
+const dificult = document.querySelector('#dificult');
+const selectWho = document.querySelectorAll(".select-who");
+const inaki = document.querySelector('#inaki');
+const wilson = document.querySelector('#wilson');
+const victor = document.querySelector('#victor');
+const usuario = document.querySelector('#quien-eres');
+const submitOne = document.querySelector('#submitOne');
 
+/*CICLOS FOR*/
+for (let i = 0; i < easyDif.length; i++) {
+    easyDif[i].addEventListener('click', function_easyDif);
+}
+
+for (let i = 0; i < selectWho.length; i++) {
+    selectWho[i].addEventListener('click', function_selectWho);
+}
 
 /*EVENTOS*/
 comenzarPartida.addEventListener("click", function_comenzarPartida)
-
+submitOne.addEventListener("click", function_play);
 
 /*FUNCIONES*/
 function function_comenzarPartida () {
@@ -18,4 +35,29 @@ function function_comenzarPartida () {
     if(page2.classList.contains("desktop-2")){
         page2.classList.replace("desktop-2", "desktop-2-show")
     }
+}
+
+function function_easyDif () {
+    if(easy.checked) {
+        localStorage.setItem("dificulty", 'easy');
+    }
+    if(dificult.checked) {
+        localStorage.setItem("dificulty", 'dificult');
+    }
+}
+
+function function_selectWho () {
+    if(inaki.checked) {
+        localStorage.setItem('character', 'Inaki');
+    }
+    if(wilson.checked) {
+        localStorage.setItem('character', 'Wilson');
+    }
+    if(victor.checked) {
+        localStorage.setItem('character', 'Victor');
+    }
+}
+
+function function_play () {
+    console.log('victor')
 }
